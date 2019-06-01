46491589721d5df274d18b2b7bb58d5d4ecc5c0d
Majed Amin <majedamin974@gmail.com>
2019-06-01 08:17:45 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,12 +4,12 @@ var playlist = {
   artist3: 'song3'
 };
 
-function updatePlaylist(playlist, artist, song) {
-  playlist.artist = song;
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist.artistName = songTitle;
   return playlist;
 }
 
-function removeFromPlaylist(playlist, artist) {
-  delete playlist.artist;
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistName;
   return playlist;
 }
